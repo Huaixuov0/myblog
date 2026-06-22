7261bf2dc2d15e5a4938837e86a62be35fd8486d
Huaixuov0 <08243426@cumt.edu.cn>
2026-06-22 23:41:35 +0800

    update site url to real vercel domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://myblog-lyart.vercel.app",
+  site: "https://myblog-chi-lilac.vercel.app",
   integrations: [sitemap()],
   build: {
     inlineStylesheets: "auto",
